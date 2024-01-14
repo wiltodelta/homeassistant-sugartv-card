063c5e03b30447dd45f3526062ac63cfb43ce2ff
Victor Kuznetsov <kuznetsov.va@gmail.com>
2024-01-14 10:09:29 -0800

    Update sugartv-card.js

--- dist/sugartv-card.js
+++ dist/sugartv-card.js
@@ -143,10 +143,10 @@ class SugarTvCard extends LitElement {
                 let delta = value - previous_value;
 
                 if (delta >= 0) {
-                    delta_str = `＋${(Math.round(delta * 10) / 10).toFixed(1)}`;
+                    delta_str = `＋${(Math.round(delta * 10) / 10)}`;
                 }
                 else {
-                    delta_str = `－${(Math.round(delta * -1 * 10) / 10).toFixed(1)}`;
+                    delta_str = `－${(Math.round(delta * -1 * 10) / 10)}`;
                 }
             }
         }
