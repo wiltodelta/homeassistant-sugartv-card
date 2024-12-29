680922550eeb285349e871f47d6db9f15bc28256
Victor Kuznetsov <kuznetsov.va@gmail.com>
2024-12-28 17:32:03 -0800

    Update sugartv-card.js

--- dist/sugartv-card.js
+++ dist/sugartv-card.js
@@ -4,7 +4,6 @@ import {
     css,
 } from "https://unpkg.com/lit-element@3.3.3/lit-element.js?module";
 import { cardStyles } from "./sugartv-card-styles.js";
-import "./sugartv-card-editor.js";
 
 // External resources configuration
 const EXTERNAL_RESOURCES = [
